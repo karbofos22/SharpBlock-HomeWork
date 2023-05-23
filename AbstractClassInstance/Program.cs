9988ec10077fb97102a550b4a3029a83a43df753
karbofos22 <karbofos22@yandex.ru>
2023-05-23 18:08:24 +0300

    added special question solution variant

---
+++ AbstractClassInstance/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace AbstractClassInstance
+{
+    public abstract class LandTransport
+    {
+    }
+
+    public class Car : LandTransport
+    {
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Type landTransport = typeof(LandTransport);
+            Type car = typeof(Car);
+
+            if (landTransport.IsAssignableFrom(car))
+            {
+                object carInstance = Activator.CreateInstance(car);
+
+                LandTransport landTransportCar = carInstance as LandTransport;
+
+                if (landTransportCar is LandTransport)
+                {
+                    Console.WriteLine("landTransportCar is an instance of LandTransport");
+                }
+                else
+                {
+                    Console.WriteLine("landTransportCar is not an instance of LandTransport");
+                }
+            }
+        }
+    }
+}
